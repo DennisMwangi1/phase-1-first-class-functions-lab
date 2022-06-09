2917fa3b1a6bc2ea4cb8df47e8f4970e384a18ed
Dennis Mwangi <dennis.kamau@student.moringaschool.com>
2022-06-09 10:50:28 +0300

    commit tree d84148f057d3458c0a2a51f5992485084733b393

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
 
-const returnFirstTwoDrivers = (drivers)=>{
-    return drivers.slice(0,2)
-}
-const returnLastTwoDrivers = (drivers)=>{
-    return drivers.slice(2)
-}
-const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
-const createFareMultiplier = (fare) =>{
+const returnFirstTwoDrivers = (drivers) =>drivers.slice(0,2)
+const returnLastTwoDrivers = (drivers) =>drivers.slice(-2)
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]    
+const createFareMultiplier = (fare)=>{
     return multiply;
     function multiply() {
-        return fare * 5;
+        return fare * 5
     }
-}
-const fareDoubler = (createFareMultiplier) =>{
-    return createFareMultiplier * 2
-}
-const fareTripler = (createFareMultiplier) =>{
-    return createFareMultiplier * 3
-}
-const selectDifferentDrivers = (drivers,returnFirstTwoDrivers) =>{
-    return returnFirstTwoDrivers(drivers);
 }
